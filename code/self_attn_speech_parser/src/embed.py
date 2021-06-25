729783667a4c08f0abef6569de6b9421b18c57cc
Eric DeMattos <s2057915@ed.ac.uk>
2021-06-25 16:08:32 +0100

    update embed indexing

--- code/self_attn_speech_parser/src/embed.py
+++ code/self_attn_speech_parser/src/embed.py
@@ -9,7 +9,7 @@ import torch
 
 def save(token_feats, speech_feats, emb_idxs, glove_vocab):
     """
-    Append the current batch of embeddings to file.
+    Append the current batch of embeddings to file; ensure batch size contains ALL instances!
     """
 
     start_idx = glove_vocab['<START>']  # 400000
@@ -24,42 +24,32 @@ def save(token_feats, speech_feats, emb_idxs, glove_vocab):
 
     # hardcode paths for now
     proj_home = '/home/s2057915/prosody_nlp/code/self_attn_speech_parser'
-    data_split = 'turn_dev'
+    data_split = 'turn_dev'  # aka ${PREFIX}
 
     # load all tokens for this split and assign each a unique ID
     with open(f"{proj_home}/stanza_data/{data_split}_indices.txt") as f:
-        tokens = [f"{token.strip()}+{data_split}+{i}" for i, token in enumerate(f)]
+       tokens = [f"{token.strip()}+{data_split}+{i}" for i, token in enumerate(f)]
 
-    ### store current batch in temp dictionary
-    ### embed_dict = dict()
-
-    ### collect number of <START> and <STOP> tokens to subtract from total
-    ### num_meta_token = 0
-
-    # write current batch to file
+    # append current batch; ensure file does not exist before running!
     with open(f"{proj_home}/{data_split}_embed.txt", "a") as f:
 
+        # TODO: move this to post processing
+        # get embedding for meta tokens
+        # f.write(f"<START> {' '.join([str(float(x)) for x in embed_concat[emb_idxs.tolist().index(start_idx)]])}\n")
+        # f.write(f"<STOP> {' '.join([str(float(x)) for x in embed_concat[emb_idxs.tolist().index(stop_idx)]])}\n")
+
         for i, idx in enumerate(emb_idxs):
-    
-            token = glove_vocab[int(idx)]
 
             # meta tokens have the same embedding; no prosodic difference
-            if token == '<START>' or token == '<STOP>':
-                print(f"skipping: {token} {' '.join([str(x) for x in embed_concat[i].tolist()[:5]])}")
+            if glove_vocab[int(idx)] == '<START>' or glove_vocab[int(idx)] == '<STOP>':
+                # print(f"skipping: {glove_vocab[int(idx)]} {' '.join([str(x) for x in embed_concat[i].tolist()[:5]])}")
                 continue
 
-            # example line: "aardvark+turn_dev+3235 -0.5 2.5 [...] 1.2 -0.7"
-            f.write(f"{tokens[i]} {' '.join([str(x) for x in embed_concat[i].tolist()])}\n")
+            # example: "aardvark+turn_dev+3235 -0.5 1.2 -0.7"
+            # f.write(f"{tokens[i]} {' '.join([str(x) for x in embed_concat[i].tolist()])}\n")
+            f.write(f"{glove_vocab[int(idx)]}+{data_split}+{i} {' '.join([str(x) for x in embed_concat[i].tolist()])}\n")
 
-
-### remove duplicate meta tokens from total count
-### vocab_size = len(tokens) - num_meta_token + 2  # add 1 for <START> and 1 for <STOP>
-
-    print(f"len(tokens) = {len(tokens)}\nglove_vocab = {len(glove_vocab.keys())}")
-
-### word2vec/fastText format requires vocabulary size and embed dimension first
-### f.write(f"{vocab_size} 1024\n")
-
-### get embedding for meta tokens
-### f.write(f"<START> {' '.join([str(x) for x in embed_concat[emb_idxs.tolist().index(start_idx)]])}\n")
-### f.write(f"<STOP> {' '.join([str(x) for x in embed_concat[emb_idxs.tolist().index(stop_idx)]])}")
+    # post processing: add vocab length and embed dimension to top of file
+    # wc -l turn_dev_embed.txt >> turn_dev_embed_fixed.txt
+    # cat turn_dev_embed.txt >> turn_dev_embed_fixed.txt
+    # rm turn_dev_embed.txt
